pinikeizman/workspace

Workspace Full Implementation
This project build from 2 layers,
BE - Node.js TS JS and Reactive Programming
FE - React TS JS

Dockerfile
@@ -1,0 +1,8 @@
+FROM node:14
+WORKDIR /opt/pini/workspace
+COPY . .
+RUN npm install
+RUN npm run build
+RUN npm prune --production
+EXPOSE 8080
+CMD [ "node", "dist/index.js"  ]

pinikeizman/notifications

Dockerfile
@@ -1,0 +1,8 @@
+FROM node:14
+WORKDIR /opt/pini/notification
+COPY . .
+RUN npm install
+RUN npm run build
+RUN npm prune --production
+EXPOSE 8080
+CMD [ "node", "dist/index.js"  ]